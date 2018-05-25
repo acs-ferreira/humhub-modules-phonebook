86d26a34920a62d8cdab8937880b9c26bef76ebd
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-25 22:15:36 +0200

    Update humhub.phonebook.js

--- resources/js/humhub.phonebook.js
+++ resources/js/humhub.phonebook.js
@@ -1,56 +1,57 @@
 humhub.module('phonebook', function(module, require, $) {
 
-    var init = function() {
-        console.log('phonebook module activated');
-    };
+  var init = function() {
+    console.log('phonebook module activated');
+  };
 
-    var hello = function() {
-        alert(module.text('hello')+' - '+module.config.username)
-    };
+  var hello = function() {
+    alert(module.text('hello')+' - '+module.config.username)
+  };
 
-    module.export({
-        //uncomment the following line in order to call the init() function also for each pjax call
-        //initOnPjaxLoad: true,
-        init: init,
-        hello: hello
-    });
+  module.export({
+    //uncomment the following line in order to call the init() function also for each pjax call
+    //initOnPjaxLoad: true,
+    init: init,
+    hello: hello
+  });
 });
 
-function gi(name)
-{
-	return document.getElementById(name);
+function gi(name) {
+  return document.getElementById(name);
 }
+
 function filter_table() {
   // Declare variables
   var input, filter, table, tr, td, i;
-  input = gi("search");
+  input = gi('search');
   filter = input.value.toLowerCase();
-  table = gi("table-data");
-  tr = table.getElementsByTagName("tr");
+  table = gi('table-data');
+  tr = table.getElementsByTagName('tr');
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    tds = tr[i].getElementsByTagName("td");
-	var sh = "none";
-	var j;
-	for(j = 0; j < tds.length; j++)
-	{
-		if(tds[j])
-		{
-		  var str = tds[j].textContent || tds[j].innerHTML;
-		  if(str.toLowerCase().indexOf(filter) > -1)
-		  {
-			sh = "";
-			break;
-		  }
-		}
-	}
-	tr[i].style.display = sh;
+    tds = tr[i].getElementsByTagName('td');
+    var sh = 'none';
+    var j;
+    for (j = 0; j < tds.length; j++) {
+      if (tds[j]) {
+        var str = tds[j].textContent || tds[j].innerHTML;
+        if (str.toLowerCase().indexOf(filter) > -1) {
+          sh = '';
+          break;
+        }
+      }
+    }
+    tr[i].style.display = sh;
   }
 }
+
 function stopEnterKey(evt) {
-        var evt = (evt) ? evt : ((event) ? event : null);
-        var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
-        if ((evt.keyCode == 13) && (node.type == "text")) { return false; }
-    }
-    document.onkeypress = stopEnterKey;
+  var evt = (evt) ? evt : ((event) ? event : null);
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+  if ((evt.keyCode == 13) && (node.type == "text")) {
+    return false;
+  }
+}
+
+document.onkeypress = stopEnterKey;
