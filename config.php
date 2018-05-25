5cc2bd96408798dd72721adc51a759adf0968d36
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-25 02:54:41 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,17 @@
 <?php
 
 return [
-	'id' => 'phonebook',
-	'class' => 'phonebook\humhub\modules\phonebook\Module',
-	'namespace' => 'phonebook\humhub\modules\phonebook',
-	'events' => [
-		[
-			'class' => \humhub\widgets\TopMenu::class,
-			'event' => \humhub\widgets\TopMenu::EVENT_INIT,
-			'callback' => ['phonebook\humhub\modules\phonebook\Events', 'onTopMenuInit'],
-		],
-//		[
-//			'class' => humhub\modules\admin\widgets\AdminMenu::class,
-//			'event' => humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-//			'callback' => ['phonebook\humhub\modules\phonebook\Events', 'onAdminMenuInit']
-//		],
-	],
-	'urlManagerRules' => [
-		'telefonliste' => 'phonebook/index'
-		 ]
-
+    'id' => 'phonebook',
+    'class' => 'phonebook\humhub\modules\phonebook\Module',
+    'namespace' => 'phonebook\humhub\modules\phonebook',
+    'events' => [
+        [
+            'class' => \humhub\widgets\TopMenu::class,
+            'event' => \humhub\widgets\TopMenu::EVENT_INIT,
+            'callback' => ['phonebook\humhub\modules\phonebook\Events', 'onTopMenuInit'],
+        ],
+        [
+            'urlManagerRules' => [
+                'telefonliste' => 'phonebook/index'
+        ]
 ];
- 
-?>
-
