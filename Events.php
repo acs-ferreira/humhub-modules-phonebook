273cc353a7bec282439d772d5f5ea81f647d78c8
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-25 15:17:50 +0200

    Update Events.php

--- Events.php
+++ Events.php
@@ -20,7 +20,7 @@ class Events extends Object
             'label' => Yii::t('PhonebookModule.base', 'Phonebook'),
             'icon' => '<i class="fa fa-phone"></i>',
             'url' => Url::to(['/phonebook/index']),
-            'sortOrder' => 1,
+            'sortOrder' => 5,
             'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'phonebook' && Yii::$app->controller->id == 'index'),
         ]);
     }
