7d624c47576e89e30829b6bf31367d9785d1314c
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-25 13:23:40 +0200

    Update index.php

--- views/index/index.php
+++ views/index/index.php
@@ -23,12 +23,12 @@ $users = User::find()
 $global_number = ''; //change this to your needs
 
 $label_field1 = 'User'; //change this to your needs
-$label_field2 = 'Country'; //change this to your needs
-$label_field3 = 'Name'; //change this to your needs
-$label_field4 = 'Phone'; //change this to your needs
-$label_field5 = 'Mobile'; //change this to your needs
-$label_field6 = 'E-mail'; //change this to your needs
-$label_field7 = 'Job Title'; //change this to your needs
+$label_field2 = 'Name'; //change this to your needs
+$label_field3 = 'Phone'; //change this to your needs
+$label_field4 = 'Mobile'; //change this to your needs
+$label_field5 = 'E-mail'; //change this to your needs
+$label_field6 = 'Job Title'; //change this to your needs
+$label_field7 = 'Country'; //change this to your needs
 $label_field8 = 'Timezone'; //change this to your needs
 
 //also change the variables = $field1 - $field9, i want to improve that later
@@ -54,25 +54,25 @@ $label_field8 = 'Timezone'; //change this to your needs
                 <thead>
                     <tr class="thead" style="vertical-align:middle; text-align:center; height:40px;">
                         <th width="5%"><?= $label_field1 ?></th>
-                        <th width="5%"><?= $label_field2 ?></th>
+                        <th width="15%"><?= $label_field2 ?></th>
                         <th width="15%"><?= $label_field3 ?></th>
                         <th width="15%"><?= $label_field4 ?></th>
                         <th width="15%"><?= $label_field5 ?></th>
                         <th width="15%"><?= $label_field6 ?></th>
-                        <th width="15%"><?= $label_field7 ?></th>
+                        <th width="5%"><?= $label_field7 ?></th>
                         <th width="15%"><?= $label_field8 ?></th>
                     </tr>
 		</thead>
                 <tbody id="table-data">
                 <?php foreach ($users as $user) :
                     $field1 = $user->getProfileImage()->getUrl();
-                    $field2 = Html::encode($user->profile->country);
-                    $field3 = Html::encode($user->profile->firstname);
-                    $field4 = Html::encode($user->profile->lastname);
-                    $field5 = Html::encode($user->profile->phone_work);
-                    $field6 = Html::encode($user->profile->phone_private);
-                    $field7 = Html::encode($user->email);
-                    $field8 = Html::encode($user->profile->title);
+                    $field2 = Html::encode($user->profile->firstname);
+                    $field3 = Html::encode($user->profile->lastname);
+                    $field4 = Html::encode($user->profile->phone_work);
+                    $field5 = Html::encode($user->profile->phone_private);
+                    $field6 = Html::encode($user->email);
+                    $field7 = Html::encode($user->profile->title);
+                    $field8 = Html::encode($user->profile->country);
                     $field9 = Html::encode($user->time_zone);
                 ?>
                     <tr class="tbody" style="text-align:center; border-top: 1px solid #eee;">
