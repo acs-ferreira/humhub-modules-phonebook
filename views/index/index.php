a6c061ee7d9fb67aeeb95220494fee766213470a
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-05-26 11:33:09 +0200

    Update index.php

--- views/index/index.php
+++ views/index/index.php
@@ -36,9 +36,9 @@ $label_field8 = 'Timezone'; //change this to your needs
 
 <div class="panel panel-default">
     <div class="panel-body">
-    <div class="panel-heading">
-            Company <strong>Phone Directory</strong>
-    </div>
+        <div class="panel-heading">
+            <?= Html::encode(Company <strong>Phone Directory</strong>); ?>
+        </div>
         <?= Html::beginForm('', 'get', ['class' => 'form-search']); ?>
         <div class="row">
             <div class="col-md-3"></div>
